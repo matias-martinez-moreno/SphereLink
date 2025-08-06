876a1ce3fdac9f142bbb9bbfde1ec8efb4526ae5
Jhon Anderson Marín Gonzalez <andersoncito@hotmail.com>
2025-08-06 10:16:24 -0500

    Eliminación de registro.
    Debido a que se trabajará con organizaciones, no es necesario hacer un register, entonces se eliminaron todas las referencias.

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),  # raíz es login
-    path('register/', views.register_view, name='register'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('create-event/', views.create_event_view, name='create_event'),
     path('profile/', views.profile_view, name='profile'),
